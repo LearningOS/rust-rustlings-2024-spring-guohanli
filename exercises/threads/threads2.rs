3e45a322962fbc7bec3e8e5bcbf5de5f890ca3e6
liguohan <2282224584@qq.com>
2024-04-29 16:55:13 +0800

    fix

--- exercises/threads/threads2.rs
+++ exercises/threads/threads2.rs
@@ -24,8 +24,7 @@ fn main() {
         let handle = thread::spawn(move || {
             thread::sleep(Duration::from_millis(250));
             // TODO: You must take an action before you update a shared value
-            let mut status_shared = status_shared.lock().unwrap();
-            (*status_shared).jobs_completed += 1;
+            status_shared.lock().unwrap().jobs_completed += 1;
         });
         handles.push(handle);
     }
