673738dd41bb41935fa2e2ef7fac996347b08f20
liguohan <2282224584@qq.com>
2024-04-29 16:46:45 +0800

    feat: finish algorithm

--- exercises/algorithm/algorithm4.rs
+++ exercises/algorithm/algorithm4.rs
@@ -3,7 +3,6 @@
 	This problem requires you to implement a basic interface for a binary tree
 */
 
-//I AM NOT DONE
 use std::cmp::Ordering;
 use std::fmt::Debug;
 
@@ -50,13 +49,34 @@ where
 
     // Insert a value into the BST
     fn insert(&mut self, value: T) {
-        //TODO
+        // If the tree is empty, create a new node and set it as the root
+        if self.root.is_none() {
+            self.root = Some(Box::new(TreeNode::new(value)));
+            return;
+        }
+
+        // Otherwise, insert the value into the tree
+        self.root.as_mut().unwrap().insert(value);
     }
 
     // Search for a value in the BST
     fn search(&self, value: T) -> bool {
-        //TODO
-        true
+        // If the tree is empty, return false
+        if self.root.is_none() {
+            return false;
+        }
+
+        // Otherwise, search for the value in the tree, hand crafted
+        let mut current = self.root.as_ref();
+        while let Some(node) = current {
+            match value.cmp(&node.value) {
+                Ordering::Less => current = node.left.as_ref(),
+                Ordering::Greater => current = node.right.as_ref(),
+                Ordering::Equal => return true,
+            }
+        }
+
+        false
     }
 }
 
@@ -66,7 +86,25 @@ where
 {
     // Insert a node into the tree
     fn insert(&mut self, value: T) {
-        //TODO
+        match value.cmp(&self.value) {
+            Ordering::Less => {
+                if let Some(ref mut left) = self.left {
+                    left.insert(value);
+                } else {
+                    self.left = Some(Box::new(TreeNode::new(value)));
+                }
+            }
+            Ordering::Greater => {
+                if let Some(ref mut right) = self.right {
+                    right.insert(value);
+                } else {
+                    self.right = Some(Box::new(TreeNode::new(value)));
+                }
+            }
+            Ordering::Equal => {
+                // Do nothing, as duplicates are not allowed
+            }
+        }
     }
 }
 
@@ -79,24 +117,24 @@ mod tests {
     fn test_insert_and_search() {
         let mut bst = BinarySearchTree::new();
 
-        
+
         assert_eq!(bst.search(1), false);
 
-        
+
         bst.insert(5);
         bst.insert(3);
         bst.insert(7);
         bst.insert(2);
         bst.insert(4);
 
-        
+
         assert_eq!(bst.search(5), true);
         assert_eq!(bst.search(3), true);
         assert_eq!(bst.search(7), true);
         assert_eq!(bst.search(2), true);
         assert_eq!(bst.search(4), true);
 
-        
+
         assert_eq!(bst.search(1), false);
         assert_eq!(bst.search(6), false);
     }
@@ -105,14 +143,14 @@ mod tests {
     fn test_insert_duplicate() {
         let mut bst = BinarySearchTree::new();
 
-        
+
         bst.insert(1);
         bst.insert(1);
 
-        
+
         assert_eq!(bst.search(1), true);
 
-        
+
         match bst.root {
             Some(ref node) => {
                 assert!(node.left.is_none());
@@ -121,6 +159,6 @@ mod tests {
             None => panic!("Root should not be None after insertion"),
         }
     }
-}    
+}
 
 
--- exercises/algorithm/algorithm9.rs
+++ exercises/algorithm/algorithm9.rs
@@ -2,7 +2,6 @@
 	heap
 	This question requires you to implement a binary heap function
 */
-// I AM NOT DONE
 
 use std::cmp::Ord;
 use std::default::Default;
@@ -38,6 +37,15 @@ where
 
     pub fn add(&mut self, value: T) {
         //TODO
+        self.items.push(value);
+        self.count += 1;
+        let mut index = self.items.len() - 1;
+        let mut top = index / 2;
+        while top > 0 && (self.comparator)(&self.items[index], &self.items[top]) {
+            self.items.swap(index, top);
+            index = top;
+            top = top / 2;
+        }
     }
 
     fn parent_idx(&self, idx: usize) -> usize {
@@ -85,7 +93,38 @@ where
 
     fn next(&mut self) -> Option<T> {
         //TODO
-		None
+        if self.is_empty() {
+            None
+        } else if self.len() == 1 {
+            self.count -= 1;
+            self.items.pop()
+        } else {
+            let last = self.len();
+            self.items.swap(1, last);
+            self.count -= 1;
+            let opt = self.items.pop();
+
+            // heapify
+            let mut index = 1usize;
+            while index < self.items.len() {
+                let left = index * 2;
+                let right = left + 1;
+                let mut smallest = index;
+                if left < self.items.len() && (self.comparator)(&self.items[left], &self.items[smallest]) {
+                    smallest = left;
+                }
+                if right < self.items.len() && (self.comparator)(&self.items[right], &self.items[smallest]) {
+                    smallest = right;
+                }
+                if index != smallest {
+                    self.items.swap(index, smallest);
+                    index = smallest;
+                } else {
+                    break;
+                }
+            }
+            opt
+        }
     }
 }
 
